4bee62d8ae4301315d1d735fb14a7f1f388f27a9
펠리페 <filip9322@gmail.com>
2023-06-19 17:59:28 +0900

    :octopus: MODEL Intersection deevice distributions, required to finish Controller model before doing migrations

---
+++ sequelize/models/intersection_device_distributions.model.js
@@ -0,0 +1,88 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = sequelize =>  {
+  class Intersection_Device_Distributions extends Model {
+    static associate (models) {
+      // associations could be defined in here
+    }
+  }
+
+  Intersection_Device_Distributions.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    controller_id: {
+      type: DataTypes.INTEGER,
+      references: 'Controllers',
+      referencesKey: 'id'
+    },
+    intersecction_type_id: {
+      type: DataTypes.INTEGER,
+      references: 'Intersection_Types',
+      referencesKey: 'id'
+    },
+    pos_1_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_2_status: {
+      type: DataTypes.INTEGER,
+       allowNull: true
+    },
+    pos_3_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_4_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_5_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_6_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_7_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_8_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_9_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_10_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_11_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pos_12_status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    user_mod: {
+      type: DataTypes.INTEGER,
+      references: 'Users',
+      referencesKey: 'id'
+    },
+    is_deleted: DataTypes.BOOLEAN
+  }, {
+    sequelize, modelName: 'Intersection_Device_Distributions'
+  });
+
+
+  return Intersection_Device_Distributions;
+}
