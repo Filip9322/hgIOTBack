2efb9375cd3fcf5c9f0a9cfb7b883a6c1ecde7a8
펠리페 <filip9322@gmail.com>
2024-12-12 17:40:00 +0900

    :peacock: CREATE  Model& Migration Event_history, missing connections and check for errors

---
+++ sequelize/migrations/20241209-event-history.js
@@ -0,0 +1,159 @@
+'use strict';
+/** @type {import ('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Event_History',{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      local_num: {
+        type: Sequelize.INTEGER,
+        references: {model: 'Local_Areas', key: 'id'},
+        allowNull: false
+      },
+      occur_time: {
+
+      },
+      inse_num: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      equi_num: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      lora_id: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      event_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: '00:00:00'
+      },
+      event_gubn: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      button_cnt: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      m358_cnt: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      m235_cnt: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      power_state: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      light_state: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      speaker_state: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      button_state: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      time1: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: '00:00:00'
+      },
+      time2: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: '00:00:00'
+      },
+      volume: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      induce: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      signal0: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      division: {
+        type: Sequelize.STRING(10),
+        allowNull: true
+      },
+      content: {
+        type: Sequelize.STRING(25),
+        allowNull: true
+      },
+      contact: {
+        type: Sequelize.STRING(30),
+        allowNull: true
+      },
+      action_content: {
+        type: Sequelize.STRING(30),
+        allowNull: true
+      },
+      action_time: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      action_name: {
+        type: Sequelize.STRING(30),
+        allowNull: true
+      },
+      picture: {
+        type: Sequelize.STRING(255),
+        allowNull: true
+      },
+      result: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        defaultValue: '미완료'
+      },
+      bigo: {
+        type: Sequelize.STRING(255),
+        allowNull: true
+      },
+      user_mod: {
+        type: Sequelize.INTEGER,
+        references: {model:'Users', key:'id'},
+        allowNull: false
+      },
+      is_deleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Event_History')
+  }
+}
---
+++ sequelize/models/event_history.js
@@ -0,0 +1,180 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = sequelize => {
+  class Event_History extends Model {
+    // associations could be defined in here
+  }
+
+  Event_History.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    local_num: {
+      type: DataTypes.INTEGER,
+      references: 'Local_Areas',
+      referencesKey: 'id',
+      allowNull: false
+    },
+    occur_time: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    inse_num: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    equi_num: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    lora_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    event_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      defaultValue: '00:00:00'
+    },
+    event_gubn: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    button_cnt: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0
+      }
+    },
+    m358_cnt: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0
+      }
+    },
+    m235_cnt: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0
+      }
+    },
+    power_state: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0
+    },
+    light_state: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 6
+      }
+    },
+    speaker_state: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 2
+      }
+    },
+    button_state: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 2
+      }
+    },
+    time1: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      defaultValue: '22:00:00'
+    },
+    time2: {
+      type: DataTypes.TIME,
+      allowNull: true,
+      defaultValue: '07:30:00'
+    },
+    volume: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 1,
+        max: 255
+      }
+    },
+    induce: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0
+    },
+    signal0: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0
+    },
+    division: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+    },
+    content: {
+      type: DataTypes.STRING(25),
+      allowNull: true
+    },
+    contact: {
+      type: DataTypes.STRING(30),
+      allowNull: true
+    },
+    action_content: {
+      type: DataTypes.STRING(30),
+      allowNull: true
+    },
+    action_time: {
+      type: DataTypes.TIME,
+      allowNull: true
+      defaultValue: '00:30:00'
+    },
+    action_name: {
+      type: DataTypes.STRING(30),
+      allowNull: true,
+    },
+    picture: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    result: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      defaultValue: '미완료'
+    },
+    bigo: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    user_mod: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  })
+}
